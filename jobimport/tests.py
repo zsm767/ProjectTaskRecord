b8cc0bad082ecf1f3876540409f4f9c86ac17aa6
Zach Melamed <zsm767@gmail.com>
2019-08-13 16:32:18 -0700

    start of some test cases

--- jobimport/tests.py
+++ jobimport/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 from .models import *
+from .forms import *
+from .views import *
 # Create your tests here.
+
+
+""" 
+Test cases for the TaskUpdateView, TaskCodes, and TaskForm classes to help with validation testing
+"""
+def createTaskCode():
+	return TaskCodes.objects.create() #need to change up this code
+
+
+class TaskUpdateViewTests( TestCase ):
+	def form_validation( self ):
+		
